5e6172f9f25955cdd0176990270c3ef5ed7af294
Lubo Sladok <bikewake@gmail.com>
2023-03-19 08:48:47 +0100

    add link to photo album, add profile icons, add description

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import PhotoAlbum from "react-photo-album";
 
+const githubLink = "https://github.com/bikewake/alfresco-document-workflows-17"
+
 const photos = [
-    { src: "/images/doc-workflows1.png", width: 667, height: 381 },
-    { src: "/images/doc-workflows2.png", width: 667, height: 376 },
-    { src: "/images/doc-workflows3.png", width: 678, height: 467 }
+    { src: "/images/doc-workflows1.png", width: 667, height: 381, href: githubLink },
+    { src: "/images/doc-workflows2.png", width: 667, height: 376, href: githubLink },
+    { src: "/images/doc-workflows3.png", width: 678, height: 467, href: githubLink }
 ];
 
 function App() {
@@ -21,7 +23,12 @@ function App() {
           BikeWake GitHub
         </a>
       </header>
-      <PhotoAlbum layout="columns" columns="2" photos={photos} />
+      <PhotoAlbum layout="columns" columns="1" photos={photos}
+      renderPhoto={({ photo, wrapperStyle, renderDefaultPhoto }) => (
+              <a href={photo.href} style={wrapperStyle} target="_blank" rel="noreferrer noopener">
+                  {renderDefaultPhoto({ wrapped: true })}
+              </a>
+          )}/>
     </div>
   );
 }
